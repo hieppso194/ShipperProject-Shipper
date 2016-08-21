1b8fc9b03fa6b3e5280e6b65c9c6d25a04d0797b
hieppso194 <hieppso194@gmail.com>
2016-08-21 16:58:36 +0700

    finish login

--- app/src/main/java/com/app/temproject/shipper/Activity/RegisterActivity.java
+++ app/src/main/java/com/app/temproject/shipper/Activity/RegisterActivity.java
@@ -11,13 +11,13 @@ import com.app.temproject.shipper.R;
 /**
  * Created by huyhoang on 12/08/2016.
  */
-public class LoginActivity extends Activity {
+public class RegisterActivity extends Activity {
 
     Button btnLogin,btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         initView();
         setEvent();
@@ -25,24 +25,23 @@ public class LoginActivity extends Activity {
 
     private void initView(){
         btnLogin = (Button) findViewById(R.id.buttondangkytaikhoan);
-        btnRegister = (Button) findViewById(R.id.buttondangky);
     }
 
     private void setEvent(){
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
+//        btnRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
